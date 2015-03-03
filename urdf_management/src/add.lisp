ead832820e2d97d6b2e7f260f0e01eaff8f2bb3d
Jannik Buckelo <jannikbu@tzi.de>
2015-03-03 11:50:17 +0100

    Fixed some bugs with updating joints

--- urdf_management/src/add.lisp
+++ urdf_management/src/add.lisp
@@ -32,10 +32,11 @@
   "Adds the links and joints descripted to the robot model."
   (let ((parsed-xml (s-xml:parse-xml-string (format nil "<container>~a</container>" xml)
                                             :output-type :xml-struct))
-        (parent-link-tree (alexandria:copy-hash-table parent-link-tree-original))
-        (link-descriptions nil)
-        (joint-descriptions nil)
-        (root-name nil))
+         (parent-link-tree (alexandria:copy-hash-table parent-link-tree-original))
+         (link-descriptions nil)
+         (new-link-names nil)
+         (joint-descriptions nil)
+         (root-name nil))
 
     ;; Get the descriptions of the joints and links from the xml
     (dolist (child (s-xml:xml-element-children parsed-xml))
@@ -48,25 +49,41 @@
          (ros-error (urdf-management) 
                     "Description contains an illegal element: ~a" child)
          (return-from add-to-robot nil))))
- 
-    ;; Check the joint descriptions and fill child-joints and joint-parents
+
+    (setf new-link-names (mapcar (lambda (link-desc)
+                                   (s-xml:xml-element-attribute link-desc :|name|))
+                                 link-descriptions))
+
+     ;; Check the joint descriptions and fill child-joints and joint-parents
     (dolist (joint-desc joint-descriptions)
-      (let ((joint-name (s-xml:xml-element-attribute joint-desc :|name|))
-            (parent-desc (cl-urdf::xml-element-child joint-desc :|parent|))
-            (child-desc (cl-urdf::xml-element-child joint-desc :|child|)))
-        (unless parent-desc
-          (ros-warn (urdf-management) "No parent for joint: ~a" joint-desc)
-          (return-from add-to-robot nil))
+      (let ((joint-name (s-xml:xml-element-attribute joint-desc :|name|)))
         (unless joint-name
-          (ros-warn (urdf-management) "No name for joint: ~a" joint-desc)
+          (ros-error (urdf-management) "No name for joint: '~a'" joint-desc)
           (return-from add-to-robot nil))
-        (let ((joint-parent (s-xml:xml-element-attribute parent-desc :|link|))
-              (joint-child (s-xml:xml-element-attribute child-desc :|link|)))
+        (let ((old-joint (gethash joint-name (joints robot)))
+              (joint-parent (get-joint-link joint-desc :|parent|))
+              (joint-child (get-joint-link joint-desc :|child|)))
+          (when (and old-joint (not joint-parent))
+            (setf joint-parent (name (parent old-joint))))
           (unless joint-parent
-            (ros-warn (urdf-management) "No parent name for joint: ~a" joint-desc)
+            (ros-error (urdf-management) "No parent name for joint '~a'" joint-name)
             (return-from add-to-robot nil))
+          (unless (or (gethash joint-parent (links robot))
+                      (member joint-parent new-link-names :test 'equal))
+            (ros-error (urdf-management) "Couldn't find parent '~a' of joint '~a'" 
+                      joint-parent joint-name)
+            (return-from add-to-robot nil))
+          (when old-joint
+            (if joint-child
+                (remhash (name (child old-joint)) parent-link-tree)
+                (setf joint-child (name (child old-joint)))))
           (unless joint-child
-            (ros-warn (urdf-management) "No child name for joint: ~a" joint-desc)
+            (ros-error (urdf-management) "No child name for joint '~a'" joint-name)
+            (return-from add-to-robot nil))
+          (unless (or (gethash joint-child (links robot))
+                      (member joint-child new-link-names :test 'equal))
+            (ros-error (urdf-management) "Couldn't find child '~a' of joint '~a'" 
+                      joint-child joint-name)
             (return-from add-to-robot nil))
           (setf (gethash joint-child parent-link-tree) joint-parent))))
 
@@ -76,20 +93,28 @@
         (ros-warn (urdf-management) "~a" err)
         (return-from add-to-robot nil))
       (setf root-name root))
+
+    ;; Check if all old links are still connected to the robot
+    (alexandria:maphash-keys (lambda (child)
+                                (unless (nth-value 1 (gethash child parent-link-tree))
+                                  (ros-error (urdf-management) 
+                                             "Link ~a not connnected to robot model anymore." 
+                                             child)
+                                  (return-from add-to-robot nil)))
+                             (links robot))
  
     ;; Create the links from the description
     (dolist (link-desc link-descriptions)
       (let* ((link-name (s-xml:xml-element-attribute link-desc :|name|))
              (old-link (gethash link-name (links robot))))
         (unless (nth-value 1 (gethash link-name parent-link-tree))
-          (progn
-            (ros-error (urdf-management) "Link ~a not connnected to robot model." link-desc)
-            (return-from add-to-robot nil)))
+          (ros-error (urdf-management) "Link ~a not connnected to robot model." link-desc)
+          (return-from add-to-robot nil))
         (if old-link
             (setf (gethash link-name (links robot)) (update-link old-link link-desc robot))
             (let ((link (create-link link-desc robot)))
               (unless link
-                (ros-warn (urdf-management) "Invalid link description: ~a" link-desc)
+                (ros-error (urdf-management) "Invalid link description: ~a" link-desc)
                 (return-from add-to-robot nil))
               (setf (gethash link-name (links robot)) link)))))
     
@@ -100,7 +125,7 @@
     (dolist (joint-desc joint-descriptions)
       (let* ((joint-name (s-xml:xml-element-attribute joint-desc :|name|))
              (old-joint (gethash joint-name (joints robot))))
-        (when old-joint
+         (when old-joint
           (setf (gethash joint-name (joints robot)) (update-joint old-joint joint-desc robot)))
         (unless old-joint
           (let ((joint (create-joint joint-desc robot)))
@@ -119,4 +144,9 @@
 (defun create-joint (joint-desc robot)
   "Parses the xml description of the joint. If it's a valid joint the joint is returned else nil."
   (let ((joint (cl-urdf::parse-xml-node :|joint| joint-desc robot)))
-    joint))    
+    joint))  
+
+(defun get-joint-link (joint-desc element)
+  (let ((link-desc (cl-urdf::xml-element-child joint-desc element)))
+    (when link-desc
+      (s-xml:xml-element-attribute link-desc :|link|))))
